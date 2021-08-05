5cf4f371c6212769f148afde53ebe59892b59fbc
Alper Can Kılıç <alper.c.kilic@gmail.com>
2021-08-05 14:59:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,8 @@ def help(updater,context):
 
 def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
-        update.message.reply_text(f'Hello {member.full_name} ,  Selamlar, ben Alper. Bu mesaj size "EkoHarita - SADECE ACİL ÇAĞRILAR - www.ekoharita.org" (https://t.me/ekoharita) grubuna katıldığınız için gönderiliyor.
+        update.message.reply_text(f'Hello {member.full_name} ,  DÜNYA VARMIŞ!
+Selamlar, ben Alper. Bu mesaj size "EkoHarita - SADECE ACİL ÇAĞRILAR - www.ekoharita.org" (https://t.me/ekoharita) grubuna katıldığınız için gönderiliyor.
 
 LÜTFEN BU MESAJI OKUMADAN GRUBUN SAĞLIĞI, KALICILIĞI ve KATILIMCILARIN BİLGİYE RAHATÇA ULAŞABİLMESİ İÇİN MESAJ ATMAYIN. (2dk) 
 -------------------------------------------------------------------------
